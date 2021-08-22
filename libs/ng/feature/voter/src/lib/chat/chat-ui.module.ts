cde5513a410e00da77570bc10b3bfb957d6ee8aa
jordems <jordems@gmail.com>
2021-08-22 15:48:20 -0700

    chore: remove materialui

--- libs/ng/feature/voter/src/lib/chat/chat-ui.module.ts
+++ libs/ng/feature/voter/src/lib/chat/chat-ui.module.ts
@@ -1,18 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { ChatUIComponent } from './chat-ui.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    FormsModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   declarations: [ChatUIComponent],
   exports: [ChatUIComponent],
 })
